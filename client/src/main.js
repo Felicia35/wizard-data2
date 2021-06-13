167c9dfe7535762df4ad355ff2112ad5ed773da8
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2021-06-13 11:25:56 +0800

    add record function

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,11 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { auth } from './plugins/firebase';
+import moment from 'moment';
 
 
 Vue.config.productionTip = false
+Vue.prototype.$moment = moment;
 
 let app;
 auth.onAuthStateChanged(() => {
