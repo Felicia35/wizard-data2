df138cd4a90913a9b0e838cfcc16f4ed7c7dd8db
ryanyen2 <ryanyen2-c@my.cityu.edu.hk>
2021-06-22 11:39:36 +0800

    change pwa icon

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    themeColor: '#000000',
+    msTileColor: '#da532c',
+    iconPaths: {
+      msTileImage: 'img/icons/mstile-150x150.png'
+    }
+  }
 }
